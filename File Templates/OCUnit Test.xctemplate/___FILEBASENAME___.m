29bfca2b98bf78ba48778394373be385399b1d73
Saul Mora <saul@magicalpanda.com>
2012-05-11 19:47:24 -0600

    Moved project templates to Project Templates folder Added simple OCUnit Test case to File Templates

---
+++ File Templates/OCUnit Test.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,28 @@
+//
+// Test Case: ___FILEBASENAME___
+// Created by ___FULLUSERNAME___ on ___DATE___
+// Copyright © ___YEAR___ by ___ORGANIZATIONNAME___
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ___FILEBASENAMEASIDENTIFIER___ : SenTestCase
+@end
+	
+	
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (void)setUp
+{
+}
+
+- (void)tearDown
+{
+}
+
+- (void)testExample
+{
+	STFail(@"Not Implemented");
+}
+
+@end
